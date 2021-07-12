564104254c2572bc1787442a32807c7e36efdc3e
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2021-07-12 15:07:40 +0530

    Update VoiceChatbot.py

--- AmeliaRobot/modules/VoiceChatbot.py
+++ AmeliaRobot/modules/VoiceChatbot.py
@@ -15,7 +15,7 @@ async def fetch(url):
     return data
 
 async def ai_amelia(url):
-    ai_name = "Amelia.mp3"
+    ai_name = "Natasha.mp3"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status == 200:
@@ -25,23 +25,23 @@ async def ai_amelia(url):
     return ai_name
 
 
-@LYCIA.on_message(filters.command("Amelia"))
+@LYCIA.on_message(filters.command("Natasha"))
 async def Lycia(_, message):
     if len(message.command) < 2:
         await message.reply_text("Lycia AI Voice Chatbot")
         return
     text = message.text.split(None, 1)[1]
     amelia = text.replace(" ", "%20")
-    m = await message.reply_text("Amelia Is Best...")
+    m = await message.reply_text("Natasha Is Best...")
     try:
         L = await fetch(f"https://api.affiliateplus.xyz/api/chatbot?message={lycia}&botname=amelia&ownername=Abhishek&user=1")
         chatbot = L["message"]
         VoiceAi = f"https://lyciavoice.herokuapp.com/lycia?text={chatbot}&lang=hi"
-        name = "amelia"
+        name = "Natasha"
     except Exception as e:
         await m.edit(str(e))
         return
-    await m.edit("Made By @itsmelegend...")
+    await m.edit("Made By @AnnihilusOP...")
     LyciaVoice = await ai_lycia(VoiceAi)
     await m.edit("Repyping...")
     await message.reply_audio(audio=LyciaVoice, title=chatbot, performer=name)
