7df735d4cad559cbdf4691383b4de882d528632c
Team @MoviZenX <84813706+saikatsingh@users.noreply.github.com>
2021-07-12 14:00:01 +0530

    Update logomaker.py

--- AmeliaRobot/modules/logomaker.py
+++ AmeliaRobot/modules/logomaker.py
@@ -36,11 +36,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByYone.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @AmeliaGroup_bot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatashaBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @camila_Support, {e}')
+   await event.reply(f'Error Report @MZX_Support, {e}')
 
 
 
@@ -76,7 +76,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByYone.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Ameliagroup_bot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatashaBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
